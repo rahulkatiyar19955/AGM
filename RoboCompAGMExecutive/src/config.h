b4fb31f2df1e90379151b776699d5fbb2841540a
Luis J <luis.manso@gmail.com>
2013-10-22 12:51:10 +0200

    RoboComp components have been moved to the RoboComp repository

--- RoboCompAGMExecutive/src/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Comment out this line if your application has a QtGui
-// #define USE_QTGUI
-#define generateTXT 1
-#define PROGRAM_NAME    "agmexecutive"
-#define SERVER_FULL_NAME   "AGMExecutive"
-
-
