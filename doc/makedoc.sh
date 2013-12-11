0b555a3893a7d9232a038685997ef37378680f36
Luis J. Manso <luis.manso@gmail.com>
2013-12-11 22:20:41 +0100

    Finished code redistribution

---
+++ doc/makedoc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf html/*
+cp tabs.css html/tabs.css
+cp AGGLEditor.png html/AGGLEditor.png
+cp AGGLEditor_A.png html/AGGLEditor_A.png
+
--- makedoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -rf doc/html/*
-make doc
-cp doc/tabs.css doc/html/tabs.css
-cp doc/AGGLEditor.png doc/html/AGGLEditor.png
-cp doc/AGGLEditor_A.png doc/html/AGGLEditor_A.png
-
