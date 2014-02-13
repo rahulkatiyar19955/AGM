8658ef6f4a75b4c2f3003a7dbd9ca01ec53f3b55
Luis J. Manso <luis.manso@gmail.com>
2014-02-13 13:23:47 +0100

    3D model viewer starts working

--- libagm/graphModelViewer.cpp
+++ libagm/graphModelViewer.cpp
@@ -184,7 +184,7 @@ QWidget *GraphModelViewer::addViewWidget(osgQt::GraphicsWindowQt* gw, osg::Node*
 
 	camera->setClearColor( osg::Vec4(0.2, 0.2, 0.6, 1.0) );
 	camera->setViewport( new osg::Viewport(0, 0, traits->width, traits->height) );
-	camera->setProjectionMatrixAsPerspective(30.0f, static_cast<double>(traits->width)/static_cast<double>(traits->height), 1.0f, 10000.0f );
+	camera->setProjectionMatrixAsPerspective(30.0f, static_cast<double>(traits->width)/static_cast<double>(traits->height), 0.01f, 1000000000.0f );
 
 	view->setSceneData( scene );
 	view->addEventHandler( new osgViewer::StatsHandler );
@@ -258,7 +258,6 @@ void GraphModelViewer::animateStep()
 		{
 			continue;
 		}
-
 		/// Spring
 		if (dist>10.*RADIUS)
 		{
