0910572002f8e9a1f36414f3bb45fccbb241fa06
ljmanso <luis.manso@gmail.com>
2014-10-05 17:19:24 +0200

    doc

--- doc/makedoc.sh
+++ doc/makedoc.sh
@@ -10,5 +10,8 @@ cp AGGLEditor.png html/AGGLEditor.png
 cp AGGLEditor_A.png html/AGGLEditor_A.png
 cp AGGLEditor_B.png html/AGGLEditor_B.png
 cp wholePicture.png html/wholePicture.png
+cp init2.png html/init2.png
+cp goal2.png html/goal2.png
+
 
 
