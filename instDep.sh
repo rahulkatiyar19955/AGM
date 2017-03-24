45ce2cdbde1b272cde1caf207939a65d9af10a47
ljmanso <luis.manso@gmail.com>
2017-03-24 12:55:14 +0100

    compatibility with older linux distros

--- instDep.sh
+++ instDep.sh
@@ -3,6 +3,9 @@
 
 sudo apt-get install python-pyparsing python-pyside pyside-tools libpython2.7-dev python-dev libboost-all-dev cmake python-imaging python-numpy pypy cython
 sudo apt-get install pypy-setuptools
+sudo apt-get install libxml2-dev
+
+
 
 git clone https://github.com/eleme/thriftpy.git
 cd thriftpy
