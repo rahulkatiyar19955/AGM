f4f27123588402d25684471cf39ca1f4d981c13b
ljmanso <luis.manso@gmail.com>
2017-03-24 12:50:48 +0100

    compatibility with older linux distros

--- instDep.sh
+++ instDep.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/sh
 
 
-sudo apt-get install python-pyparsing python-pyside pyside-tools libpython2.7-dev python-dev libboost-all-dev cmake python-imaging python-numpy pypy pypy-setuptools cython
+sudo apt-get install python-pyparsing python-pyside pyside-tools libpython2.7-dev python-dev libboost-all-dev cmake python-imaging python-numpy pypy cython
+sudo apt-get install pypy-setuptools
 
 git clone https://github.com/eleme/thriftpy.git
 cd thriftpy
