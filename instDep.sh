01254964b6fc4104af92f3a0c499fc5d898d93c5
Luis J. Manso <luis.manso@gmail.com>
2017-06-04 02:03:59 +0200

    include missing package in instDep.sh

--- instDep.sh
+++ instDep.sh
@@ -2,7 +2,7 @@
 
 
 sudo apt-get install python-pyparsing python-pyside pyside-tools libpython2.7-dev python-dev libboost-all-dev cmake python-imaging python-numpy pypy cython
-sudo apt-get install pypy-setuptools
+sudo apt-get install pypy-setuptools python-setuptools
 sudo apt-get install libxml2-dev
 
 
