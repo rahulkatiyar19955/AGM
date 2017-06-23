767549ca7d2a0282a549b937afeec6a794ac309b
Luis J. Manso <luis.manso@gmail.com>
2017-06-23 13:56:28 +0200

    add dependencies

--- instDep.sh
+++ instDep.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 
-sudo apt-get install python-pyparsing python-pyside pyside-tools libpython2.7-dev python-dev libboost-all-dev cmake python-imaging python-numpy pypy cython
+sudo apt-get install python-pyparsing python-pyside pyside-tools libpython2.7-dev python-dev libboost-all-dev cmake python-imaging python-numpy pypy cython libgsl-dev libopenscenegraph-dev
 sudo apt-get install pypy-setuptools python-setuptools
 sudo apt-get install libxml2-dev
 
