6e7ce9de6d3bfbfd8b45a07b183bb17c49d7fd9e
Luis J. Manso <luis.manso@gmail.com>
2017-09-01 14:27:01 +0200

    add dependence

--- instDep.sh
+++ instDep.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-sudo apt-get install python-pyparsing python-pyside pyside-tools libpython2.7-dev python-dev libboost-all-dev cmake python-imaging python-numpy pypy cython libgsl-dev libopenscenegraph-dev pypy-setuptools python-setuptools libxml2-dev python-pygraphviz python-networkx
+sudo apt-get install python-pyparsing python-pyside pyside-tools libpython2.7-dev python-dev libboost-all-dev cmake python-imaging python-numpy pypy cython libgsl-dev libopenscenegraph-dev pypy-setuptools python-setuptools libxml2-dev python-pygraphviz python-networkx pypy-dev
 
 git clone https://github.com/eleme/thriftpy.git
 cd thriftpy
